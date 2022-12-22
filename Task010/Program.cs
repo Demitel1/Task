4e45c097d84eb95273629b7ccaebc304ea1f4e70
jntGCC <demiteli.png.@gmail.com>
2022-12-23 01:12:24 +0300

    исправили ошибку

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -2,9 +2,9 @@
 
 Console.Write("Введите трехзначное число: ");
 int firstnumber = int.Parse(Console.ReadLine()!);
-int secondnumber = firstnumber % 100 / 10;
+int secondnumber = (firstnumber % 100) / 10;
 
-if (firstnumber >= 100)
+if (firstnumber >= 100 && firstnumber <= 999)
 {
     Console.WriteLine($"Из числа {firstnumber} второе число {secondnumber}");
 }
--- Task013/Program.cs
+++ Task013/Program.cs
@@ -2,13 +2,13 @@
 
 Console.Write("Введите трехзначное число: ");
 int firstnumber = int.Parse(Console.ReadLine()!);
-int secondnumber = (firstnumber / 100) % 10;
+int secondnumber = (firstnumber / 10) % 10;
 
 if (firstnumber >= 100)
 {
-    Console.WriteLine($"Из числа {firstnumber} второе число {secondnumber}");
+    Console.WriteLine($"Из числа {firstnumber} третье число {secondnumber}");
 }
 else
 {
-    Console.WriteLine("В введенном числе нет третьего числа");
+    Console.WriteLine("В введенном числе нет третей цифры.");
 }
