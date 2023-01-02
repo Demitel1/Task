f6f557c203dc186fb38a6fbd0c13062862966f1a
jntGCC <demiteli.png.@gmail.com>
2023-01-02 19:52:27 +0300

    21 задача

---
+++ Task19/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+Console.Write("Введите проверяемое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int secondnumber1(int number)
+{
+    int result = number / 10000;
+    return result;
+}
+int firstnumber1 = secondnumber1(number);
+
+int secondnumber2(int number)
+{
+    int number1 = (number % 10) * 100;
+    int number2 = ((number % 100) / 10) * 10;
+    int number3 = (number % 1000) / 100; 
+    int result = number1 + number2 + number3;
+    return result;
+}
+int firstnumber2 = secondnumber2(number);
+
+if (firstnumber1 == firstnumber2)
+{
+    Console.Write("Число является палиндромом.");
+}
+else
+{
+    Console.Write("Число не является палиндромом.");
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+double getDistanceCoordinate(int userAx, int userAy, int userAz, int userBx, int userBy, int userBz)
+{
+    double result = Math.Sqrt(Math.Pow((userAx - userBx), 2) + Math.Pow((userAy - userBy), 2) + Math.Pow((userAz - userBz), 2));
+    return result;
+}
+
+int userAx = getUserValue("Введите значение Ax: ");
+int userAy = getUserValue("Введите значение Ay: ");
+int userAz = getUserValue("Введите значение Az: ");
+
+int userBx = getUserValue("Введите значение Bx: ");
+int userBy = getUserValue("Введите значение By: ");
+int userBz = getUserValue("Введите значение Bz: ");
+
+double distance = getDistanceCoordinate(userAx, userAy, userAz, userBx, userBy, userBz);
+Console.WriteLine($"Расстояние между точками A и B: {distance}");
