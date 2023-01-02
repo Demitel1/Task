4382adbe4c9ca963b13ec1f8cdd26a978b59b835
jntGCC <demiteli.png.@gmail.com>
2023-01-02 19:56:28 +0300

    23 задача

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int number1 = getUserValue("Введите число: ");
+int number2 = 1; 
+
+while (number2 <= number1)
+{
+    if(number2 < number1)
+    {Console.Write(Math.Pow(number2, 3) +  ", ");
+    number2++;}
+    else if(number2 == number1)
+    {Console.Write(Math.Pow(number2, 3) + ".");
+    number2++;}
+}
