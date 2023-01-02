19548f1f3a7847fb2778dc8f75dcdbf532ebed25
jntGCC <demiteli.png.@gmail.com>
2023-01-02 19:40:24 +0300

    Закончили

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -2,10 +2,20 @@
 
 Console.Write("Введите трехзначное число: ");
 int firstnumber = int.Parse(Console.ReadLine()!);
-int secondnumber = (firstnumber / 10) % 10;
+int secondnumber = firstnumber % 10;
 
-if (firstnumber >= 100)
+if (firstnumber >= 100 && firstnumber <= 999)
 {
+    Console.WriteLine($"Из числа {firstnumber} третье число {secondnumber}");
+}
+else if (firstnumber >= 1000 && firstnumber <=9999)
+{
+    secondnumber = (firstnumber / 10)% 10;
+    Console.WriteLine($"Из числа {firstnumber} третье число {secondnumber}");
+}
+else if (firstnumber >= 10000 && firstnumber <=99999)
+{
+    secondnumber = (firstnumber / 100)% 10;
     Console.WriteLine($"Из числа {firstnumber} третье число {secondnumber}");
 }
 else
