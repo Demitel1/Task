792e78df63c3eb938af2d136740bfa8d5bf059ab
jntGCC <demiteli.png.@gmail.com>
2023-01-20 18:28:47 +0300

    Задача решена

---
+++ Task025/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int number1 = getUserValue("Введите первое число: ");
+int number2 = getUserValue("Введите второе число: ");
+
+if(number1 > 0 && number2 > 0)
+{
+    Console.Write(Math.Pow(number1, number2));
+}
