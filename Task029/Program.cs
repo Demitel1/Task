23b439a5ebf431f5eb0c9253f1274968d19d65bf
jntGCC <demiteli.png.@gmail.com>
2023-01-23 00:01:16 +0300

    Задача решена

---
+++ Task029/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] getArrayRandom(int length)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(1, 50+1);   
+    }
+    return arr;
+}
+
+void showArray(int[] array)
+{
+    Console.Write($"[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i != array.Length - 1)
+        {Console.Write($"{array[i]},");}
+        else
+        {Console.Write($"{array[i]}]");}
+    }
+}
+
+int[] array = getArrayRandom(8);
+
+showArray(array);
