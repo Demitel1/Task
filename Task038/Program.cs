b46dec2ec54fc01976344f8bfb9c402cd200d0b7
jntGCC <demiteli.png.@gmail.com>
2023-01-28 15:00:28 +0300

    Все задачи решены

--- Task038/Program.cs
+++ Task038/Program.cs
@@ -5,48 +5,16 @@ int[] getArrayRandom(int length)
     int[] array = new int[length];
     for (int i = 0; i < length; i++)
     {
-        array[i] = new Random().Next(-5, 4+1);   
+        array[i] = new Random().Next(1, 9+1);   
     }
     return array;
 }
 
-int getMinNumber(int[] array)
+int getResult(int[] array)
 {
-    int min = 0; int max = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > min)
-        {
-            min = array[i];
-        }
-        else
-        {
-            max = array[i];
-        }
-    }
-    return min;
-}
-
-int getMaxNumber(int[] array)
-{
-    int min = 0; int max = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > min)
-        {
-            max = array[i];
-        }
-        else
-        {
-            min = array[i];
-        }
-    }
-    return max;
-}
-
-int getResult(int max; int min)
-{
-    int result = getMaxNumber - getMinNumber;;
+    int min = array.Min();
+    int max = array.Max();
+    int result = max - min;
     return result;
 }
 
@@ -62,10 +30,7 @@ void showArray(int[] array)
     }
 }
 
-int result = getResult(max;min);
-int min = getMinNumber(array);
-int max = getMaxNumber(array);
-int[] array = getArrayRandom(5);
-int sum = getSum(array);
+int[] array = getArrayRandom(4);
+int sum = getResult(array);
 showArray(array);
-Console.WriteLine($" cумма нечетных чисел = {result}");
+Console.WriteLine($" -> разница между max и min = {sum}");
