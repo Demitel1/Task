2cfebe946f77f37f3ae425e0c508152bc76b1cd0
jntGCC <demiteli.png.@gmail.com>
2023-01-28 21:50:37 +0300

    конец 5 практической

---
+++ Task041/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int[] getUserValue(string message)
+{
+    Console.Write(message);
+    int[] result = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return result;
+}
+
+int[] number = getUserValue("Введите числа через пробел: ");
+
+int getResultNumber(int number)
+{
+    int count = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;   
+}
+
+int count = getResultNumber(number);
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
---
+++ Task043/Program.cs
@@ -0,0 +1,27 @@
+﻿int getUserValue(string message)
+{
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+ 
+double getX(double k1,double b1,double k2,double b2)
+{
+    var x = (b1 - b2) / (k1 - k2);
+    return x;
+}
+
+double getY(double k1,double b1,double x)
+{
+    var y = k1 * x + b1;
+    return y;
+}
+
+var k1 = getUserValue("Введите k1: ");
+var b1 = getUserValue("Введите b1: ");
+var k2 = getUserValue("Введите k2: ");
+var b2 = getUserValue("Введите b2: ");
+var x = getX(k1,b1,k2,b2);
+var y = getY(k1,b1,x);
+
+Console.WriteLine($"Пересечение прямых в точке: ({x};{y})");
