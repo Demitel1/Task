ec90948d859981dce4a0fc10c7022de6e8b5fa2e
jntGCC <demiteli.png.@gmail.com>
2023-01-30 16:49:49 +0300

    Задача решена

--- Task041/Program.cs
+++ Task041/Program.cs
@@ -1,28 +1,8 @@
 ﻿Console.Clear();
 
-int[] getUserValue(string message)
+int[] getUserValue(string messege)
 {
-    Console.Write(message);
-    int[] result = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    Console.Write(messege);
+    int result = int.Parse(Console.ReadLine()!);
     return result;
 }
-
-int[] number = getUserValue("Введите числа через пробел: ");
-
-int[] getResultNumber(int number)
-{
-    int[] count = 0;
-    for (int i = 0; i < number.Length; i++)
-    {
-        if (number > 0)
-        {
-            count++;
-        }
-        
-    }
-    return count;  
-}
-
-int[] count = getResultNumber(number);
-
-Console.WriteLine($"Кол-во элементов > 0: {count}");
--- Task043/Program.cs
+++ Task043/Program.cs
@@ -1,28 +1,34 @@
 ﻿Console.Clear();
 
-double getUserValue(string message)
+double getUserValue(String message)
 {
     Console.Write(message);
     int result = int.Parse(Console.ReadLine()!);
     return result;
 }
 
-double getResultX(double b1, double b2, double k1, double k2)
+double getPointX(double b1, double k1, double b2, double k2)
 {
     double x = (-b2 + b1)/(-k1 + k2);
     return x;
 }
 
-double getResultY(double b1, double k1, double x)
+double getPointY(double x, double b2, double k2)
 {
-    double y = k1 * x + b1;
+    double y = k2 * x + b2;
     return y;
 }
 
-double k1 = getUserValue("Введите значение k1: ");
-double b1 = getUserValue("Введите значение b1: ");
-double k2 = getUserValue("Введите значение k2: ");
-double b2 = getUserValue("Введите значение b2: ");
-double x = getResultX(b1,b2,k1,k2);
-double y = getResultY(b1,k1,x); 
-Console.Write($"Пересечение двух прямых в точке с координатами X: {x}, Y: {y}");
+void PrintPoints(double x, double y)
+{
+    Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+}
+
+double b1 = getUserValue("введите значение b1: ");
+double k1 = getUserValue("введите число k1: ");
+double b2 = getUserValue("введите значение b2: ");
+double k2 = getUserValue("введите число k2: ");
+
+double x = getPointX(b1,k1,b2,k2);
+double y = getPointY(x,b2,k2);
+PrintPoints(x,y);
