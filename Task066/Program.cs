3d2fdc48e986273f02d52a49b7cf24efb6713a5d
jntGCC <demiteli.png.@gmail.com>
2023-02-01 21:34:30 +0300

    Задача решена

---
+++ Task066/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int AllSumNumbs(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + AllSumNumbs(m, n - 1);
+}
+
+int m = getUserValue("Введите M:");
+int n = getUserValue("Введите N:");
+
+Console.WriteLine($"Сумма элементов от {m} до {n} = {AllSumNumbs(m, n)}");
