31369bc1b02118e9e6ce8b1bb60a6876f9620c5a
jntGCC <demiteli.png.@gmail.com>
2023-02-01 21:29:48 +0300

    Задача решена

---
+++ Task064/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int IntervalofNumbs(int n, int m)
+{
+    if (n == m)
+        return n;
+    else if (n < 1)
+        {Console.WriteLine("Ввели не положительное число");}
+    else
+        {Console.Write($"{IntervalofNumbs(n, m + 1)}, ");}
+    return m;
+}
+
+int n = getUserValue("Введите число: ");
+int m = 1;
+
+Console.WriteLine(IntervalofNumbs(n, m));
