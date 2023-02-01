0c53fb546d0643b829e0f5373df377212ce76287
jntGCC <demiteli.png.@gmail.com>
2023-02-01 21:39:25 +0300

    конец блока

---
+++ Task068/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+
+int getUserValue(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Formula(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Formula(m - 1, 1);
+    else
+        return Formula(m - 1, Formula(m, n - 1));
+}
+
+int m = getUserValue("Введите M: ");
+int n = getUserValue("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Formula(m, n)}");
