38ed3e2587aac61722117e4b5f2aeadf46f6240c
jntGCC <demiteli.png.@gmail.com>
2023-02-01 17:47:21 +0300

    Задача решена

---
+++ Task050/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+
+int rows = getUserValue("Введите количество строк: ");
+int colums = getUserValue("Введите количество столбцов: ");
+int [,] numbers = new int [10,10];
+
+int getUserValue(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(1, 10);
+            }   
+        }
+}
+
+void showdDoubleArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintResult(int[,] numbers)
+{
+    if (rows > numbers.GetLength(0) || colums > numbers.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine($"значение элемента {rows} строки и {colums} столбца равно {numbers[rows-1,colums-1]}");
+    }
+}
+
+FillArrayRandomNumbers(numbers);
+showdDoubleArray(numbers);
+PrintResult(numbers);
---
+++ Task052/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+void ArithmeticMean(int[,] numbers, int rows)
+{
+    double avarage = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            avarage = (avarage + numbers[i, j]);
+        }
+        avarage = avarage / rows;
+        Console.Write(avarage + "; "); 
+    }
+    return; 
+}
+
+void DoubleArrays(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void showdDoubleArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int rows = getUserValue("Введите количество строк: ");
+int colums = getUserValue("Введите количество столбцов: ");
+int[,] numbers = new int[rows, colums];
+
+DoubleArrays(numbers);
+showdDoubleArray(numbers);
+ArithmeticMean(numbers,rows);
